ebb88fd7d6f7aa550090dfb1713db61adcff9b46
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-12-26 09:06:05 -0400

    Created Admin Seeder

---
+++ database/seeders/CreateAdmin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class CreateAdmin extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create(['name' => 'Admin', 'email' => 'admin@admin.com', 'password' => bcrypt('admin')]);
+    }
+}
