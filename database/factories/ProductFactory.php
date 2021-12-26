84a440991c76e006699755d8dcdda395e7d27d41
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-12-26 09:49:04 -0400

    Updated Product Factory add user_id random user

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ProductFactory extends Factory
@@ -14,6 +15,7 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
+            'user_id' => $this->faker->randomElement(User::pluck('id')),
             'name' => $this->faker->name(),
             'description' => $this->faker->text(),
             'status' => 'enable'
