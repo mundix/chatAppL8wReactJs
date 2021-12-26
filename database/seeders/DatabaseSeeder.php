27a3d506e384053b1e82baf530435b3ada07e6c4
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-12-26 09:41:05 -0400

    Add CreateProduct Seeder to DatabaseSeeder Class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-           CreateAdmin::class,
+//           CreateAdmin::class,
+           CreateProducts::class,
         ]);
     }
 }
