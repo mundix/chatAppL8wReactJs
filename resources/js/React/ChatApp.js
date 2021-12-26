0ea61dfa03408cc3929ff1e04df3a8fb4f37d43d
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-12-26 10:41:25 -0400

    Refactored ChatApp add states and html and compiled

--- resources/js/React/ChatApp.js
+++ resources/js/React/ChatApp.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from "react";
 import ReactDOM from 'react-dom';
+// import {ChatRoomItem} from "./Components/ChatRoomItem";
+// import {ChatItem} from "./Components/ChatItem";
 
 export const ChatApp = () => {
+
+    const [chatRooms, setChatRooms] = useState([]);
+    const [showChats, setShowChats] = useState(false);
+    const [chats, setChats] = useState([]);
+
+    const handleBackButton = () => {
+        getRooms();
+    }
+
+    const handleRoomChats = async (room) => {
+        await axios.get(`http://abcmio-old.lndo.site/es/my/chats/${room.id}`)
+            .then( resp => {
+                setChats(resp.data.chats);
+            });
+        setShowChats(true);
+    }
+
+    const getRooms = async() => {
+        setShowChats(false);
+        await axios.get('http://abcmio-old.lndo.site/es/my/chats')
+            .then(resp => {
+                const data = resp.data;
+                setChatRooms(data.data);
+            });
+    }
+
+    useEffect(() => {
+        getRooms();
+    }, []);
+
     return (
         <>
-            <h1>Chat App</h1>
+            <h1>Chat App In React Js</h1>
+            <hr/>
+            <div className="row">
+                <div className="col-4 chat-room pt-1" id="chat-room">
+                    {
+                        !showChats && chatRooms && (
+                            <h2>Chat Rooms</h2>
+                        )
+                    }
+                    {
+                        showChats && chats && (<button className="btn btn-primary mb-1" onClick={handleBackButton}>Volver</button>)
+                    }
+                    {
+                        showChats && chats && (
+                            <h2>Users Chats</h2>
+                        )
+                    }
+                </div>
+                <div className="col-8 chat-body" id="chat-body">
+                    <h2>Body Chat</h2>
+                </div>
+            </div>
         </>
     )
 }
